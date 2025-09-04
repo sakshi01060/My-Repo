25e79c0d3b8aa87bbf69564cd8a195a193a2468c
sakshi01060 <sakshi01060@gmail.com>
2025-09-04 20:56:45 +0530

    Add files via upload

---
+++ Weather.py
@@ -0,0 +1,18 @@
+import requests
+
+city_name = input("Enter the city:-")
+
+API_Key = "6c95410239msh6901ad4bccd5692p116e14jsn89c74385f4ab"
+url = "https://weatherapi-com.p.rapidapi.com/alerts.json"
+
+headers = {
+    "X-RapidAPI-Key": API_Key,
+    "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+}
+
+params = {"q": city_name}
+
+response = requests.get(url, headers=headers, params=params)
+
+data = response.json()
+print(data)
